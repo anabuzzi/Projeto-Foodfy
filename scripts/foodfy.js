7135de487a7834122c779ef90e2b05209e067f2a
anabuzzi <barcelosbuzzi@gmail.com>
2020-07-16 19:34:42 -0300

    foodyf final

--- scripts/foodfy.js
+++ scripts/foodfy.js
@@ -1,15 +1,6 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
-const imgId = document.querySelectorAll('.img')
-var modal = document.getElementById(".myModal");
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onClick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-  }
+const img = document.querySelector('img')
 
 for (let card of cards) {
       
@@ -17,14 +8,16 @@ for (let card of cards) {
       const imgId = card.getAttribute("id")
       modalOverlay.classList.add('active')     
       modalOverlay.querySelector("iframe").src = `/assets/${imgId}.png`
-      
-    })
+      })
   }
   
 document.querySelector('.maximize-modal').addEventListener ("click", function() {
     modalOverlay.classList.add('maximize')
-    
+    img.getAttribute.add('width')
+
+  
 }) 
+
 
 document.querySelector('.minimize-modal').addEventListener ("click", function() {
     modalOverlay.classList.remove('maximize')
