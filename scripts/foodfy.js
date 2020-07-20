6d689a0fce2e783d5664a89fbb0c628ac7588a59
anabuzzi <barcelosbuzzi@gmail.com>
2020-07-20 13:41:16 -0300

    Final Foodfy - Modal content

--- scripts/foodfy.js
+++ scripts/foodfy.js
@@ -2,18 +2,28 @@ const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 const img = document.querySelector('img')
 
+
+
 for (let card of cards) {
       
-      card.addEventListener("click", function () {
-      const imgId = card.getAttribute("id")
-      modalOverlay.classList.add('active')     
-      modalOverlay.querySelector("iframe").src = `/assets/${imgId}.png`
-      })
+    card.addEventListener("click", function () {
+    const imgId = card.getAttribute("id")
+    modalOverlay.classList.add('active')     
+    modalOverlay.querySelector("iframe").src = `/assets/${imgId}.png` 
+
+ 
+    modalOverlay.querySelector('.iframecontent').textContent = card.querySelector('.cardtitle').textContent
+    modalOverlay.querySelector('.iframecontent2').textContent= card.querySelector('.infocard').textContent
+      
+
+
+       
+    })
   }
   
 document.querySelector('.maximize-modal').addEventListener ("click", function() {
     modalOverlay.classList.add('maximize')
-    img.getAttribute.add('width')
+    
 
   
 }) 
